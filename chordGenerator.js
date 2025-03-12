35b3447ec5f29ce3f093db65a02d882ccd967858
vanolson <vanolson@uchicago.edu>
2025-03-12 15:28:57 -0400

    Mostly there

---
+++ chordGenerator.js
@@ -0,0 +1,363 @@
+// Code for generating the next chord
+class ChordGenerator {
+    // Some globals
+    notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+    major = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+    minor = ['i', 'ii', 'iii', 'iv', 'v', 'vi'];
+    diminished = ['vii'];
+    dominant = ['V', 'II', 'III', 'VI', 'VII', 'bVII'];
+    probabilityMapping = ChordGenerator.probabilityMapping();
+
+    constructor(key = 'C', starterChord = 'I') {
+        this.key = this.notes.includes(key) ? key : 'C';
+        this.chordMapping = ChordGenerator.generateChordMapping();
+        this.nextChord = this.chordMapping.hasOwnProperty(starterChord) ? starterChord : 'I';
+        this.nextDrums;
+        this.level = 2; // range is 0-4 (inclusive)
+        this.probability;
+        this.setProbability();
+        this.bassNote;
+        this.setBassNote();
+        this.chordNoteArray;
+        this.setChordNoteArray();
+    }
+
+    setNextChord() {
+        const current = this.nextChord;
+        const rand = Math.random();
+        console.log(rand)
+        for (let [type, p] of Object.entries(this.probabilityMapping[this.level])) {
+            if (rand <= p) {
+                if (this.chordMapping.hasOwnProperty(current) && this.chordMapping[current].hasOwnProperty(type)) {
+                    const chordOptions = this.chordMapping[current][type];
+                    const index = Math.floor(Math.random() * chordOptions.length);
+                    this.nextChord = chordOptions[index];
+                }
+                else {
+                    // Prevent it from getting stuck if I forgot a case
+                    this.nextChord = 'I';
+                }
+                break;
+            }
+        }
+
+        // change key in certain cases
+        if (this.nextChord === 'keyChange') {
+            this.key = this.intervalNoteToLetterNote(
+                'IV', this.intervalNoteToLetterNote(current, this.key)
+            );
+            this.nextChord = 'I';
+        }
+
+        this.setBassNote();
+        this.setChordNoteArray();
+    }
+
+    setBassNote() {
+        // Always in the second octave
+        this.bassNote = `${this.intervalNoteToLetterNote(this.nextChord, this.key)}2`;
+    }
+
+    setChordNoteArray() {
+        let noteNums;
+        let seventh;
+        let ninth;
+        let justTheChord = this.nextChord;
+        if (this.nextChord[0] == 'b') {
+            justTheChord = this.nextChord.substring(1);
+        }
+        else if (this.nextChord[0] == '#') {
+            justTheChord = this.nextChord.substring(1);
+        }
+        if (this.major.includes(justTheChord)) {
+            noteNums = [0, 4, 7];
+            if (this.dominant.includes(justTheChord)) {
+                seventh = 10;
+            }
+            else {
+                seventh = 11;
+            }
+            ninth = 2;
+        }
+        else if (this.minor.includes(justTheChord)) {
+            noteNums = [0, 3, 7];
+            seventh = 10;
+            ninth = 2;
+        }
+        else if (this.diminished.includes(justTheChord)) {
+            noteNums = [0, 3, 6];
+            seventh = Math.random() < 0.5 ? 10 : 9;
+            ninth = 2;
+        }
+
+        let chordNotes = this.noteNumbersToNoteArray(noteNums);
+
+        // Use these notes in the 3rd-5th octaves
+        this.chordNoteArray = chordNotes.map((n) => [`${n}3`, `${n}4`, `${n}5`]).flat()
+
+        // Potentially add additives (7/9) on the 5th octave
+        let additives = []
+        if (Math.random() > this.probability.normal) {
+            additives.push(seventh);
+            console.log(`added interval ${seventh}`);
+        }
+        if (Math.random() > this.probability.funky) {
+            additives.push(ninth);
+            console.log(`added interval ${ninth}`);
+        }
+        let additiveNotes = this.noteNumbersToNoteArray(additives);
+        this.chordNoteArray = this.chordNoteArray.concat(additiveNotes.map((n) => `${n}5`));
+        console.log("CNA", this.chordNoteArray);
+    }
+
+    setProbability() {
+        this.probability = this.probabilityMapping[this.level];
+    }
+
+    weirder() {
+        if (this.level > 0) {
+            this.level -= 1;
+        }
+        this.setProbability();
+    }
+
+    normaler() {
+        if (this.level < 4) {
+            this.level += 1;
+        }
+        this.setProbability();
+    }
+
+    // Input should be zero-indexed (i.e. the I chord is 0, 4, 7)
+    noteNumbersToNoteArray(noteNums) {
+        const notes = this.notes;
+        const noteIndex = notes.indexOf(this.intervalNoteToLetterNote(this.nextChord, this.key));
+        console.log(noteNums, notes.length, noteIndex);
+        console.log((noteIndex + noteNums[0]) % notes.length);
+
+        return noteNums.map((n) => notes[(noteIndex + n) % notes.length])
+    }
+
+    intervalNoteToLetterNote(intervalNote, key) {
+        // We're just gonna use sharps
+        const notes = this.notes;
+
+        let flatSharpDelta = 0;
+        let justTheNote = intervalNote
+        if (intervalNote[0] == 'b') {
+            flatSharpDelta = -1;
+            justTheNote = intervalNote.substring(1);
+        }
+        else if (intervalNote[0] == '#') {
+            flatSharpDelta = 1;
+            justTheNote = intervalNote.substring(1);
+        }
+
+        const rootNoteMapping = {
+            'I': 0,
+            'II': 2,
+            'III': 4,
+            'IV': 5,
+            'V': 7,
+            'VI': 9,
+            'VII': 11,
+        };
+
+        const noteSemitones = rootNoteMapping[justTheNote.toUpperCase()] + flatSharpDelta;
+
+        const index = (notes.indexOf(key) + noteSemitones) % notes.length;
+        return notes[index];
+    }
+
+    static probabilityMapping() {
+        function probObj(normal, funky, weird) {
+            return {
+                normal: normal,
+                funky: funky,
+                weird: weird,
+            }
+        }
+        return {
+            0: probObj(.20, .75, 1.00),
+            1: probObj(.40, .83, 1.00),
+            2: probObj(.60, .90, 1.00), // This is the default
+            3: probObj(.75, .95, 1.00),
+            4: probObj(.90, .98, 1.00),
+        }
+    }
+
+    static generateChordMapping() {
+        /* The data structure */
+        const chordMapping = new Object();
+
+        const chordList = [
+            'I', 'i', 'ii', 'II', 'III', 'iii', 'iv', 'IV', 'V', 'VI', 'vi', 'VII', 'vii',
+            'bii', 'bII', 'biii', 'bIII', 'bv', 'bV', 'bVI', 'bvi', 'bVII', 'bvii'
+        ];
+    
+        /* Standard major scale */
+
+        /* I CHORD */
+        chordMapping['I'] = {
+            normal: ['V', 'IV', 'vi', 'ii', 'iii'],
+            funky: ['iv', 'vii', 'II', 'III', 'VI', 'VII'],
+        };
+
+        /* ii CHORD */
+        chordMapping['ii'] = {
+            normal: ['V', 'iii', 'IV', 'vi', 'I'],
+            funky: ['II', 'III', 'vii']
+        }
+
+        /* iii CHORD */
+        chordMapping['iii'] = {
+            normal: ['vi', 'IV', 'I', 'ii', 'vii'],
+            funky: ['vii', 'III', 'VI'],
+        }
+
+        /* IV CHORD */
+        chordMapping['IV'] = {
+            normal: ['I', 'V', 'vi'],
+            funky: ['iv', 'III', 'vii'],
+        }
+
+        /* V CHORD */
+        chordMapping['V'] = {
+            normal: ['I', 'IV', 'vi', 'ii'],
+            funky: ['VI', 'iv', 'VI', 'vii', 'III'],
+        }
+
+        /* vi CHORD */
+        chordMapping['vi'] = {
+            normal: ['I', 'IV'],
+            funky: ['I', 'VI', 'vii'],
+        }
+
+        /* vii CHORD */
+        chordMapping['vii'] = {
+            normal: ['I'],
+            funky: ['vii', 'V', 'vii'],
+        }
+
+        /* Soft support for minor key */
+
+        /* i CHORD */
+        chordMapping['i'] = {
+            normal: ['I', 'iv', 'V'],
+            funky: ['v', 'bVI', 'bVII'],
+        }
+
+        /* bIII CHORD */
+        chordMapping['bIII'] = {
+            normal: ['i', 'iv', 'v', 'bVII', 'bVI'],
+            funky: ['keyChange', 'bIII', 'V', 'bVII'],
+        }
+
+        /* iv CHORD */
+        // This also is just a minor four leading to I
+        chordMapping['iv'] = {
+            normal: ['I', 'i'],
+            funky: ['IV', 'V', 'vi', 'v', 'bVII'],
+        }
+
+        /* v CHORD */
+        chordMapping['v'] = {
+            normal: ['i', 'iv', 'bVII', 'bVI'],
+            funky: ['bIII', 'V', 'bVII'],
+        }
+
+        /* bVI CHORD */
+        chordMapping['bVI'] = {
+            normal: ['i', 'v', 'V', 'bIII'],
+            funky: ['keyChange', 'iv', 'bVII'],
+        }
+
+        /* bVII CHORD */
+        chordMapping['bVII'] = {
+            normal: ['i', 'V', 'v', 'bIII'],
+            funky: ['keyChange', 'iv', 'bVI'],
+        }
+
+
+        /* Subdominants */
+
+        /* II CHORD */
+        chordMapping['II'] = {
+            normal: ['V', 'ii'],
+            funky: ['keyChange', 'III', 'I'],
+        }
+
+        /* III CHORD */
+        chordMapping['III'] = {
+            normal: ['vi', 'IV'],
+            funky: ['keyChange'],
+        }
+
+        /* VI CHORD */
+        chordMapping['VI'] = {
+            normal: ['iii', 'V'],
+            funky: ['keyChange'],
+        }
+
+        /* VI CHORD */
+        chordMapping['VII'] = {
+            normal: ['iii', 'III', 'V'],
+            funky: ['keyChange'],
+        }
+
+        /* Flats */
+        chordMapping['bii'] = {
+            normal: ['I', 'biii'],
+            funky: ['bvi', 'V'],
+        }
+
+        chordMapping['bII'] = {
+            normal: ['I', 'bIII', 'V'],
+            funky: ['keyChange'],
+        }
+
+        chordMapping['biii'] = {
+            normal: ['I', 'v', 'ii', 'iv'],
+            funky: ['bvi', 'V'],
+        }
+
+        chordMapping['bIII'] = {
+            normal: ['I', 'bII', 'V'],
+            funky: ['keyChange'],
+        }
+
+        chordMapping['bv'] = {
+            normal: ['I', 'V', 'IV'],
+            funky: ['bvi', 'v'],
+        }
+
+        chordMapping['bV'] = {
+            normal: ['I', 'bVI', 'V'],
+            funky: ['keyChange'],
+        }
+
+        chordMapping['bvi'] = {
+            normal: ['I', 'bii', 'V'],
+            funky: ['biii', 'bVI', 'vi'],
+        }
+
+        chordMapping['bvii'] = {
+            normal: ['i', 'vii', 'I'],
+            funky: ['biii', 'bII', 'bVI'],
+        }
+
+        const notIncluded = chordList.filter((c) => !Object.keys(chordMapping).includes(c));
+        if (notIncluded.length > 0) {
+            console.log("Note to self: chords not included are", notIncluded);
+        }
+
+        // 'weird' chords are the ones not included in normal or funky
+        for (let [chord, info] of Object.entries(chordMapping)) {
+            info.weird = chordList.filter((c) => chord != c && !info.normal.includes(c) && !info.funky.includes(c))
+        }
+
+        console.log(chordMapping)
+        return chordMapping;
+    }
+    
+}
---
+++ script.js
@@ -0,0 +1,189 @@
+// Global state variables
+let chordGenerator;
+
+// Global Tone.js constants
+const drumset = new Tone.Players({
+    kick: 'https://tonejs.github.io/audio/drum-samples/CR78/kick.mp3',
+    snare: 'https://tonejs.github.io/audio/drum-samples/CR78/snare.mp3',
+    hihat: 'https://tonejs.github.io/audio/drum-samples/CR78/hihat.mp3',
+    tom1: 'https://tonejs.github.io/audio/drum-samples/CR78/tom1.mp3',
+    tom2: 'https://tonejs.github.io/audio/drum-samples/CR78/tom2.mp3',
+    tom3: 'https://tonejs.github.io/audio/drum-samples/CR78/tom3.mp3',
+});
+
+// As eighth notes
+const drumPattern = {
+    kick: [[1, 0, 0, 0], [.3, .1, 0, 0], [0, .8, .7, 0], [1, 0, 0, .2]].flat(),
+    snare: [[0, 0, 0, 0], [.9, 0, 0, 0], [0, 0, 0, 0], [1, 0, .2, .5]].flat(),
+    hihat: [[1, 1, .9, 1], [1, 0.9, 0.95, 1], [1, 1, 1, 1], [0.8, 1, 1, 1]].flat(),
+    tom1: [[0, 0, 0, 0], [0, 0, .75, .9], [0, 0, 0, 0], [0.95, 0, 0, 0]].flat(),
+    tom2: [[0, 0, 0.3, 0], [0, 0, 0, 0], [0.4, 0.5, 0, 0], [0, 0.95, 0, 0]].flat(),
+    tom3: [[0, 0, 0, 0], [0, 0.3, 0, 0], [0, 0, 0, 0], [0, 0, 0.95, 0.2]].flat(),
+}
+
+const drumPatternTranspose = [];
+for (i in [...Array(16).keys()]) {
+    let obj = {};
+    for (let [inst, seq] of Object.entries(drumPattern)) {
+        obj[inst] = seq[i];
+    }
+    drumPatternTranspose.push(obj);
+}
+
+const drumVol = new Tone.Volume(-10);
+
+const drumReverb = new Tone.Reverb(2, 0.1);
+drumReverb.set({
+    wet: 0.7,
+})
+
+const drumLowpass = new Tone.Filter(8000, "lowpass");
+
+drumset.connect(drumVol);
+drumVol.connect(drumReverb);
+drumReverb.connect(drumLowpass);
+drumLowpass.toDestination();
+
+/* SYNTH */
+
+const synth = new Tone.PolySynth();
+
+const settings = {
+    volume: -12,
+    detune: 0,
+    portamento: 0.01,
+    envelope: {
+        attack: 0.3,
+        attackCurve: "sine",
+        decay: 0.4,
+        decayCurve: "exponential",
+        release: 0.6,
+        releaseCurve: "sine",
+        sustain: 1
+    },
+    oscillator: {
+        partialCount: 2,
+        partials: [1, 2],
+        phase: 2,
+        type: "sine",
+        count: 5,
+        spread: 4,
+    },
+}
+
+synth.set(settings);
+
+const vol = new Tone.Volume(-12);
+
+const delay = new Tone.Delay("8n", 0.5);
+
+const reverb = new Tone.Reverb(5, 0.1);
+
+const phaser = new Tone.Phaser({
+	"frequency" : 0.2,
+	"octaves" : 5,
+	"baseFrequency" : 100
+});
+
+const lowpass = new Tone.Filter(4000, "lowpass");
+
+const autoFilter = new Tone.AutoFilter({
+    frequency: 0.3,
+    type: 'sine',
+    depth: 0.8,
+    baseFrequency: 200,
+    octaves: 5,
+    filter: {
+        type: 'lowpass',
+        rolloff: -12,
+        Q: 1
+    }
+}).start();
+
+const channel = new Tone.Channel({channelCount: 2});
+
+synth.connect(vol);
+vol.connect(reverb);
+reverb.connect(phaser);
+phaser.connect(lowpass);
+lowpass.connect(autoFilter);
+autoFilter.connect(channel);
+channel.toDestination();
+
+const bass = new Tone.Synth();
+bassReverb = new Tone.Reverb(2, 0.1);
+bassReverb.set({
+    wet: 0.7,
+})
+
+bass.connect(bassReverb)
+bassReverb.toDestination();
+
+Tone.Transport.bpm.value = 64;
+new Tone.Loop(playSynth, '1n').start();
+new Tone.Sequence(playDrum, drumPatternTranspose, '8n').start();
+
+// Set up responsive buttons, chord generator
+function initializeComponents() {
+    chordGenerator = new ChordGenerator('C', 'I');
+
+    const playButton = nn.get('.play-button');
+    playButton.addEventListener('click', triggerStartStop);
+
+    const leftButton = nn.get('.left .button');
+    const rightButton = nn.get('.right .button');
+
+    leftButton.addEventListener('click', weirder);
+    rightButton.addEventListener('click', normaler);
+}
+
+function triggerStartStop() {
+    if (Tone.Transport.state === 'stopped') {
+        console.log('started!');
+        Tone.getTransport().start();
+    } 
+    else {
+        console.log('stopped!');
+        Tone.getTransport().stop();
+    }
+    nn.get('.title h1')
+        .css({'text-shadow': '0px 6px 3px var(--center-color)'});
+}
+
+function playSynth(time) {
+    console.log(time);
+    console.log(chordGenerator)
+    console.log('BN', chordGenerator.bassNote)
+    console.log('CNA', chordGenerator.chordNoteArray)
+    bass.triggerAttackRelease(chordGenerator.bassNote, '1n', time)
+    synth.triggerAttackRelease(chordGenerator.chordNoteArray, '1n', time);
+    chordGenerator.setNextChord();
+}
+
+function playDrum(time, notes) {
+    // console.log(time);
+    // console.log(notes);
+    rands = Object.keys(notes).map(() => Math.random());
+    let i = 0;
+    for (const inst in notes) {
+        if (rands[i] < notes[inst]) {
+            drumset.player(inst).start();
+        }
+        i++;
+    }
+}
+
+function weirder() {
+    chordGenerator.weirder();
+    console.log('weirder!');
+    // Edit the css for the title
+    nn.get('.title h1')
+        .css({'text-shadow': '-6px 6px 3px var(--left-color)'});
+}
+
+function normaler() {
+    chordGenerator.normaler();
+    console.log('normaler!');
+    nn.get('.title h1')
+        .css({'text-shadow': '6px 6px 3px var(--right-color)'});
+}
